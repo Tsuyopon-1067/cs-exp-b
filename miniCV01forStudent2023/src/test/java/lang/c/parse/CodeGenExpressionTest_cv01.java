72d84d170cea167aaf303cab97965f5be79ac7e7
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-10-23 13:35:40 +0900

    :white_check_mark: test cv01

--- miniCV01forStudent2023/src/test/java/lang/c/parse/CodeGenExpressionTest_cv01.java
+++ miniCV01forStudent2023/src/test/java/lang/c/parse/CodeGenExpressionTest_cv01.java
@@ -87,4 +87,102 @@ public class CodeGenExpressionTest_cv01 {
 
     // Please copy and paste the above code and add the specified test case to the following
     // 上記のコードをコピー＆ペーストし、指定されたテストケースを以下に追加してください。
+
+    @Test
+    public void codeGenExpressionADDADDSubTerm() throws FatalErrorException {
+        inputStream.setInputString("13+7-3");
+        String expected[] = {
+            "MOV #13, (R6)+",
+            "MOV #7, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "ADD R1, R0",
+            "MOV R0, (R6)+",
+            "MOV #3, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "SUB R0, R1",
+            "MOV R1, (R6)+",
+        };
+
+        // Check only code portion, not validate comments
+        CParseRule rule = new Expression(cpContext);
+        helper.checkCodeGen(expected, rule, cpContext);
+    }
+
+    @Test
+    public void codeGenExpressionADD3Term() throws FatalErrorException {
+        inputStream.setInputString("13+7+3");
+        String expected[] = {
+            "MOV #13, (R6)+",
+            "MOV #7, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "ADD R1, R0",
+            "MOV R0, (R6)+",
+            "MOV #3, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "ADD R1, R0",
+            "MOV R0, (R6)+",
+        };
+
+        // Check only code portion, not validate comments
+        CParseRule rule = new Expression(cpContext);
+        helper.checkCodeGen(expected, rule, cpContext);
+    }
+
+    @Test
+    public void codeGenExpressionADD3CommentTerm() throws FatalErrorException {
+        inputStream.setInputString("13+/*comment*/7+3");
+        String expected[] = {
+            "MOV #13, (R6)+",
+            "MOV #7, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "ADD R1, R0",
+            "MOV R0, (R6)+",
+            "MOV #3, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "ADD R1, R0",
+            "MOV R0, (R6)+",
+        };
+
+        // Check only code portion, not validate comments
+        CParseRule rule = new Expression(cpContext);
+        helper.checkCodeGen(expected, rule, cpContext);
+    }
+
+    @Test
+    public void codeGenExpressionADDSub5CommentTerm() throws FatalErrorException {
+        inputStream.setInputString("1+2-3+4-5");
+        String expected[] = {
+            "MOV #1, (R6)+",
+            "MOV #2, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "ADD R1, R0",
+            "MOV R0, (R6)+",
+            "MOV #3, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "SUB R0, R1",
+            "MOV R1, (R6)+",
+            "MOV #4, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "ADD R1, R0",
+            "MOV R0, (R6)+",
+            "MOV #5, (R6)+",
+            "MOV -(R6), R0",
+            "MOV -(R6), R1",
+            "SUB R0, R1",
+            "MOV R1, (R6)+",
+        };
+
+        // Check only code portion, not validate comments
+        CParseRule rule = new Expression(cpContext);
+        helper.checkCodeGen(expected, rule, cpContext);
+    }
 }
