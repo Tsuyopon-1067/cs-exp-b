b78bf19d2418cfa144d45db3836459b1053cca25
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-10-24 13:36:44 +0900

    :fire:  reset: cv02

--- miniCV01forStudent2023/src/main/java/lang/c/test.c
+++ miniCV01forStudent2023/src/main/java/lang/c/test.c
@@ -1,1 +1,19 @@
-13 + 7 + 2
+- 100  // 実装してください
+    13 +
+    7 -
+    2    //  readSimpleTokenSub()
+    100  // 数字
+    + -  // plus minus 記号
+      // LINE_COMMENT
+      13 +
+    7 +
+    2
+    /* COMMENT_START AND COMMENT_LINE1
+       COMMENT_LINE2
+       COMMENT_LINE3
+       COMMENT_LINE4 AND COMMENT_END */
+    13 +
+    7 +
+    2
+    /***/ 123 /*/12/*/ 34 /*/56/*/ 78  // 123 34 78 が出てくるはず
+/***         // 閉じていないコメントはEOFが出るはず
