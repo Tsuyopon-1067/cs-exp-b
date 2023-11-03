69fba8214e6adf5a500e317651cd4ef9a9bc7c43
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-11-03 19:54:52 +0900

    :heavy_plus_sign: misc: kokoko

---
+++ miniCV02forStudent2023/kokokonolabs-log/ws-history/2023-10-31_12.08.25/workspace/src/main/java/lang/c/CTokenizerStateConst.java
@@ -0,0 +1,9 @@
+package lang.c;
+
+import lang.SimpleToken;
+
+public class CTokenizerStateConst {
+	public static final int ST_INIT		= 0;	//
+	public static final int ST_EOF		= 1;	//
+	public static final int ST_ILL		= 2;	//
+}
---
+++ miniCV02forStudent2023/kokokonolabs-log/ws-history/2023-10-31_12.14.25/workspace/src/main/java/lang/c/CTokenizerStateConst.java
@@ -0,0 +1,16 @@
+package lang.c;
+
+import lang.SimpleToken;
+
+public class CTokenizerStateConst {
+	public static final int ST_INIT		= 0;	// 初期状態
+	public static final int ST_EOF		= 1;	//
+	public static final int ST_ILL		= 2;	//
+	public static final int ST_ZERO_HEX_OCT		= 3;	//
+	public static final int ST_PLUS		= 4;	//
+	public static final int ST_MINUS		= 5;	//
+	public static final int ST_MUL		= 6;	//
+	public static final int ST_DIV		= 7;	//
+	public static final int ST_AMP		= 11;	//
+	public static final int ST_DEC		= 12;	//
+}
