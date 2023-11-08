5ed73dfe38d5c35903c05e76fd4b3fc44252320d
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-11-08 22:24:52 +0900

    :pencil2: fix: comment

--- miniCV03forStudent2023/src/main/java/lang/c/parse/Expression.java
+++ miniCV03forStudent2023/src/main/java/lang/c/parse/Expression.java
@@ -6,7 +6,7 @@ import lang.*;
 import lang.c.*;
 
 public class Expression extends CParseRule {
-	// expression ::= term { expressionAdd }
+	// expression ::= term { expressionAdd | expressionSub }
 	CParseRule expression;
 
 	public Expression(CParseContext pcx) {
