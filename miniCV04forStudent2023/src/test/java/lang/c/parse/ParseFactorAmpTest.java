df4b4a7028aaef6b7748c93987c9a84529475cc7
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-11-25 01:03:17 +0900

    :white_check_mark: add: missing test case

---
+++ miniCV04forStudent2023/src/test/java/lang/c/parse/ParseFactorAmpTest.java
@@ -0,0 +1,72 @@
+package lang.c.parse;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import lang.FatalErrorException;
+import lang.IOContext;
+import lang.InputStreamForTest;
+import lang.PrintStreamForTest;
+import lang.c.CParseContext;
+import lang.c.CToken;
+import lang.c.CTokenRule;
+import lang.c.CTokenizer;
+
+public class ParseFactorAmpTest {
+    InputStreamForTest inputStream;
+    PrintStreamForTest outputStream;
+    PrintStreamForTest errorOutputStream;
+    CTokenizer tokenizer;
+    IOContext context;
+    CParseContext cpContext;
+
+    @Before
+    public void setUp() {
+        inputStream = new InputStreamForTest();
+        outputStream = new PrintStreamForTest(System.out);
+        errorOutputStream = new PrintStreamForTest(System.err);
+        context = new IOContext(inputStream, outputStream, errorOutputStream);
+        tokenizer = new CTokenizer(new CTokenRule());
+        cpContext = new CParseContext(context, tokenizer);
+    }
+
+    @After
+    public void tearDown() {
+        inputStream = null;
+        outputStream = null;
+        errorOutputStream = null;
+        tokenizer = null;
+        context = null;
+        cpContext = null;
+    }
+
+    void resetEnvironment() {
+        tearDown();
+        setUp();
+    }
+
+    @Test
+    public void factarAmpTest()  {
+        String[] testDataArr = {"&*i_pa"};
+        for ( String testData: testDataArr ) {
+            resetEnvironment();
+            inputStream.setInputString(testData);
+            CToken firstToken = tokenizer.getNextToken(cpContext);
+            assertThat("Failed with " + testData, Program.isFirst(firstToken), is(true));
+            Program cp = new Program(cpContext);
+
+            try {
+                cp.parse(cpContext);
+                fail("Error should be invoked");
+            } catch ( FatalErrorException e ) {
+                assertThat(e.getMessage(), containsString("&の後はNumberかPrimaryです"));
+            }
+        }
+    }
+}
