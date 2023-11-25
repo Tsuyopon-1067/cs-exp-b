0f22e4347d6088f2f25c11d870e3ab13186b11de
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-11-25 21:02:16 +0900

    :heavy_plus_sign: add: cv06

---
+++ miniCV06forStudent2023/src/test/java/lang/c/parse/HelperTestStrMsg.java
@@ -0,0 +1,16 @@
+package lang.c.parse;
+
+public class HelperTestStrMsg {
+    private String testStr;
+    private String msg;
+    public HelperTestStrMsg(String str, String msg) {
+        this.testStr = str;
+        this.msg = msg;
+    }
+    public String getTestStr() {
+        return testStr;
+    }
+    public String getMsg() {
+        return msg;
+    }
+}
