a0dfca522fd6b33b2494518217e9af0bc4a47e4a
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-12-01 12:26:54 +0900

    :sparkles: feat: additional statement types

--- miniCV07forStudent2023/src/main/java/lang/c/parse/statement/Statement.java
+++ miniCV07forStudent2023/src/main/java/lang/c/parse/statement/Statement.java
@@ -6,14 +6,19 @@ import lang.*;
 import lang.c.*;
 
 public class Statement extends CParseRule {
-    // statement       ::= statementAssign
+    // statement ::= statementAssign | statementInput | statementOutput | statementIf | statementWhile | statementBlock
     CParseRule statmentAssign;
 
 	public Statement(CParseContext pcx) {
 	}
 
 	public static boolean isFirst(CToken tk) {
-        return StatementAssign.isFirst(tk);
+        return StatementAssign.isFirst(tk)
+			|| StatementInput.isFirst(tk)
+			|| StatementOutput.isFirst(tk)
+			|| StatementIf.isFirst(tk)
+			|| StatementWhile.isFirst(tk)
+			|| StatementBlock.isFirst(tk);
 	}
 
 	public void parse(CParseContext pcx) throws FatalErrorException {
