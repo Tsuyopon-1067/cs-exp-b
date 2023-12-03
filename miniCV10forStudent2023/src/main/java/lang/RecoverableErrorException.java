26d5cf7fb8417fa9197469a7eb4565b0d1c4ec0c
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-12-04 03:09:18 +0900

    :heavy_plus_sign: add: cv10

---
+++ miniCV10forStudent2023/src/main/java/lang/RecoverableErrorException.java
@@ -0,0 +1,7 @@
+package lang;
+
+public class RecoverableErrorException extends FatalErrorException {
+    public RecoverableErrorException(String message) {
+        super(message);
+    }
+}
---
+++ miniCV10forStudent2023/src/main/java/lang/c/parse/condition/ConditionGE.java
@@ -0,0 +1,35 @@
+package lang.c.parse.condition;
+
+import java.io.PrintStream;
+
+import lang.*;
+import lang.c.*;
+import lang.c.parse.AbstractConditionOperator;
+
+public class ConditionGE extends AbstractConditionOperator {
+	//conditionGE ::= GE expression
+
+	public ConditionGE(CParseContext pcx, CParseRule left, CToken op) {
+		super(pcx, left, op);
+	}
+
+	@Override
+	public void codeGen(CParseContext pcx) throws FatalErrorException {
+		PrintStream o = pcx.getIOContext().getOutStream();
+		o.println(";;; condition >= (compare) starts");
+		if (left != null && right != null) {
+			left.codeGen(pcx);
+			right.codeGen(pcx);
+			int seq = pcx.getSeqId();
+			o.println("\tMOV\t-(R6), R0\t; ConditionGE: 2数を取り出して、比べる");
+			o.println("\tMOV\t-(R6), R1\t; ConditionGE:");
+			o.println("\tMOV\t#0x0001, R2\t; ConditionGE: set true");
+			o.println("\tADD\t#1, R0\t; ConditionGE: R0に1を加える");
+			o.println("\tCMP\tR1, R0\t; ConditionGE: R1>=R0 = (R1+1)>R0 = R0-(R1+1)<0");
+			o.println("\tBRN\tGE" + seq + "\t; ConditionGE: N=1のときGE[seq]へジャンプ");
+			o.println("\tCLR\tR2\t\t; ConditionGE: set false");
+			o.println("GE" + seq + ":\tMOV\tR2, (R6)+\t; ConditionGE:");
+		}
+		o.println(";;;condition >= (compare) completes");
+	}
+}
