3c91960dd39d52235b8671438a4da548e87da33b
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-12-11 13:41:20 +0900

    :white_check_mark: add: missing teset case

--- miniCV06forStudent2023/src/test/java/lang/c/CTokenizerConditionTest.java
+++ miniCV06forStudent2023/src/test/java/lang/c/CTokenizerConditionTest.java
@@ -122,4 +122,34 @@ public class CTokenizerConditionTest {
 
     // Please copy and paste the above code and add the specified test case to the following
     // 追加するテストケース："<<=>>=!===", "!<="
+    public void condition6Token() {
+        String testString = "<<=>>=!===";
+        inputStream.setInputString(testString);
+        CToken token1 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 1", token1, CToken.TK_LT, "<", 1, 1);
+        CToken token2 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 2", token2, CToken.TK_LE, "<=", 1, 2);
+        CToken token3 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 3", token3, CToken.TK_GT, ">", 1, 4);
+        CToken token4 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 4", token4, CToken.TK_GE, ">=", 1, 5);
+        CToken token5 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 5", token5, CToken.TK_NE, "!=", 1, 7);
+        CToken token6 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 6", token6, CToken.TK_EQ, "==", 1, 9);
+        CToken token7 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 7", token7, CToken.TK_EOF, "end_of_file", 1, 11);
+    }
+
+    @Test
+    public void conditionILL() {
+        String testString = "!<=";
+        inputStream.setInputString(testString);
+        CToken token1 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 1", token1, CToken.TK_ILL, "!", 1, 1);
+        CToken token2 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 2", token2, CToken.TK_LE, "<=", 1, 2);
+        CToken token3 = tokenizer.getNextToken(cpContext);
+        helper.checkToken("token 3", token3, CToken.TK_EOF, "end_of_file", 1, 4);
+    }
 }
