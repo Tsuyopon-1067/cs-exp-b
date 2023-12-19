c497ed3704421ea994a6c7f1771204aa685ec102
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-12-19 10:29:40 +0900

    :white_check_mark: update: test code

--- miniCV09forStudent2023/src/main/java/lang/c/test.c
+++ miniCV09forStudent2023/src/main/java/lang/c/test.c
@@ -1,19 +1,31 @@
-- 100  // 実装してください
-    13 +
-    7 -
-    2    //  readSimpleTokenSub()
-    100  // 数字
-    + -  // plus minus 記号
-      // LINE_COMMENT
-      13 +
-    7 +
-    2
-    /* COMMENT_START AND COMMENT_LINE1
-       COMMENT_LINE2
-       COMMENT_LINE3
-       COMMENT_LINE4 AND COMMENT_END */
-    13 +
-    7 +
-    2
-    /***/ 123 /*/12/*/ 34 /*/56/*/ 78  // 123 34 78 が出てくるはず
-/***         // 閉じていないコメントはEOFが出るはず
+if true) {
+  c_a = 4
+  i_b = 4;
+  i_c = 4;
+}
+while (3 < true {
+  i_d = 4
+  i_e = 4;
+  i_f = 4;
+}
+if (3 true) {
+  c_g = 4
+  i_h = 4;
+  i_i = 4;
+}
+i_ac = 1+2+3+;
+i_ad = 1*2*3*;
+input c_g;
+input 3;
+input c_g;
+output c_h;
+output 1+2+;
+c_i = i_j * ip_k;
+c_i = i_j * ia_m;
+c_i = i_j * &true;
+ip_o = &ip_p + &i_q;
+i_r = *ip_s + *i_t;
+i_u = ia_v[true] + ia_w[];
+i_x = true * false;
+i_aa = 3+3+;
+i_ab = 3+3+
