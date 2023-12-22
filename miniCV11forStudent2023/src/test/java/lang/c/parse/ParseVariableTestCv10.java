fa67663becd0d68f1a24bfc34e46a0c54dcafe50
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-12-22 10:40:18 +0900

    :white_check_mark: disable: ignore test

--- miniCV11forStudent2023/src/test/java/lang/c/parse/ParseVariableTestCv10.java
+++ miniCV11forStudent2023/src/test/java/lang/c/parse/ParseVariableTestCv10.java
@@ -58,6 +58,7 @@ public class ParseVariableTestCv10 {
         setUp();
     }
 
+    @Ignore
     @Test
     public void parseVariableCorrect() throws FatalErrorException {
         String[] testDataArr = {
