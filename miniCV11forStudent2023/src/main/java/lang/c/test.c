9337475a86c5a449e0e76e400b99289722fa83a9
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-01-11 00:44:56 +0900

    :white_check_mark: update test.c

--- miniCV11forStudent2023/src/main/java/lang/c/test.c
+++ miniCV11forStudent2023/src/main/java/lang/c/test.c
@@ -1,19 +1,11 @@
-- 100  // 実装してください
-    13 +
-    7 -
-    2    //  readSimpleTokenSub()
-    100  // 数字
-    + -  // plus minus 記号
-      // LINE_COMMENT
-      13 +
-    7 +
-    2
-    /* COMMENT_START AND COMMENT_LINE1
-       COMMENT_LINE2
-       COMMENT_LINE3
-       COMMENT_LINE4 AND COMMENT_END */
-    13 +
-    7 +
-    2
-    /***/ 123 /*/12/*/ 34 /*/56/*/ 78  // 123 34 78 が出てくるはず
-/***         // 閉じていないコメントはEOFが出るはず
+const int a = 1;
+int a;
+{
+    a = 1
+}
+{
+    const int a = 1;
+    int b;
+    const int b = 1;
+    a = 2;
+}
