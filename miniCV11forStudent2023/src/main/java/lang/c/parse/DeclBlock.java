a37af39b80dcf4d8e168f4c88d59893b0b17e043
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-01-12 13:02:10 +0900

    :bug: fix: code gen

--- miniCV11forStudent2023/src/main/java/lang/c/parse/DeclBlock.java
+++ miniCV11forStudent2023/src/main/java/lang/c/parse/DeclBlock.java
@@ -72,6 +72,8 @@ public class DeclBlock extends CParseRule {
 			}
 		}
 
+		o.println("\tMOV\tR4, (R6)+\t; DeclItem: フレームポインタをスタックに退避する");
+		o.println("\tMOV\tR6, R4\t; DeclItem: 現在のスタックの値をフレームポインタにする");
 		o.println("\tADD\t#" + variableSize + ", R6\t; DeclItem: 局所変数の領域を確保する");
 
 		if (statmentList != null) {
@@ -79,7 +81,8 @@ public class DeclBlock extends CParseRule {
 				statment.codeGen(pcx);
 			}
 		}
-		o.println("\tSUB\t#" + variableSize + ", R6\t; DeclItem: 局所変数の分だけスタックポインタを戻す");
+		o.println("\tMOV\tR4, R6\t; DeclItem: 局所変数の領域を開放する");
+		o.println("\tMOV\t-(R6), R4\t; DeclItem: 旧フレームポインタを復帰する");
 		o.println(";;; DeclBlock completes");
 	}
 }
