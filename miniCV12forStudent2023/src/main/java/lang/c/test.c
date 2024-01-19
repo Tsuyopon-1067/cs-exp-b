c4db5900846ebedf5a2f696d74dd167df37b14f6
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-01-19 20:26:23 +0900

    :white_check_mark: add: test code

--- miniCV12forStudent2023/src/main/java/lang/c/test.c
+++ miniCV12forStudent2023/src/main/java/lang/c/test.c
@@ -1,19 +1,28 @@
-- 100  // 実装してください
-    13 +
-    7 -
-    2    //  readSimpleTokenSub()
-    100  // 数字
-    + -  // plus minus 記号
-      // LINE_COMMENT
-      13 +
-    7 +
-    2
-    /* COMMENT_START AND COMMENT_LINE1
-       COMMENT_LINE2
-       COMMENT_LINE3
-       COMMENT_LINE4 AND COMMENT_END */
-    13 +
-    7 +
-    2
-    /***/ 123 /*/12/*/ 34 /*/56/*/ 78  // 123 34 78 が出てくるはず
-/***         // 閉じていないコメントはEOFが出るはず
+void hoge();
+
+func int test() {
+  call no_exist_function();
+  return 1;
+}
+
+func int main() {
+  int a, b;
+  a = test();
+  a = b();
+  return 0;
+}
+
+func void hoge() {
+  int a;
+  //a = hoge();
+  a = main();
+  call main();
+  call a();
+}
+
+func void return_type_error() {
+  return 0;
+}
+
+func int no_return() {
+}
