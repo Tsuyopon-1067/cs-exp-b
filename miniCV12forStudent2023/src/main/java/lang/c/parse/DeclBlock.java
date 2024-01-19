9cbec4c498d47add3c9eedb21ec25c9edc4f7639
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-01-19 16:07:30 +0900

    :bug: fix: return label

--- miniCV12forStudent2023/src/main/java/lang/c/parse/DeclBlock.java
+++ miniCV12forStudent2023/src/main/java/lang/c/parse/DeclBlock.java
@@ -13,8 +13,14 @@ public class DeclBlock extends CParseRule {
 	ArrayDeque<CParseRule> declareList = new ArrayDeque<CParseRule>();
 	ArrayDeque<CParseRule> statmentList = new ArrayDeque<CParseRule>();
 	int variableSize = 0;
+	private String returnLabel;
 
 	public DeclBlock(CParseContext pcx) {
+		this.returnLabel = "";
+	}
+
+	public DeclBlock(CParseContext pcx, String returnLabel) {
+		this.returnLabel = returnLabel;
 	}
 
 	public static boolean isFirst(CToken tk) {
@@ -35,7 +41,7 @@ public class DeclBlock extends CParseRule {
 		}
 
 		while (Statement.isFirst(tk)) {
-			statmentList.add(new Statement(pcx));
+			statmentList.add(new Statement(pcx, returnLabel));
 			statmentList.getLast().parse(pcx);
 			ct = pcx.getTokenizer();
 			tk = ct.getCurrentToken(pcx);
